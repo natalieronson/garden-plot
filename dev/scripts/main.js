695b8770c387b10bd3e97e1d70cb0fc61e1ab506
natalieronson <natalie.ronson@gmail.com>
2018-05-31 12:16:15 -0400

    Testing noun project images

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -33,7 +33,7 @@ let num = 0;
             // $("#container1").append(`<div class="resizeDiv"></div>`)
             $("#container1").append(`
             <div class="resizeDiv">
-                <img class="veg-image" id='veg${num}' crossOrigin='anonymous'>
+                <img class="veg-image" id='veg${num}' crossorigin='anonymous'>
             </div>`);
             $(`#veg${num}`).attr("src", image)
 
